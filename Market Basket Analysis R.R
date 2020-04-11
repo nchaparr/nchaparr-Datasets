c9f2e577a01d872049173f15e482c71a5e604afa
unknown <nosull@yahoo.com>
2020-04-11 15:43:27 -0700

    ran r script for Masket Basket Analysis

---
+++ Market Basket Analysis R.R
@@ -0,0 +1,13 @@
+setwd("C:/Program Files/R/Datasets/Chapter08")
+install.packages("arules")
+library(arules)
+groceries<-read.transactions("groceries.csv", sep=",")
+summary(groceries)
+inspect(groceries[1:5])
+itemFrequencyPlot(groceries, support=0.1)
+itemFrequencyPlot(groceries, topN=20)
+install.packages("apriori")
+groceryrules<-apriori(groceries,parameter=list(support=.006, confidence=0.25, minlen=2))
+inspect(sort(groceryrules, by="lift")[1:20]
+
+        
